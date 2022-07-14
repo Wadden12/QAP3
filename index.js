41f0632de484d8e991d8e57a38a1107a364b5153
Mike Wadden <mike.wadden@keyin.com>
2022-07-13 22:53:52 -0230

    finished post request

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+// Author: Mike Wadden
+// Date: July 13,2022
+// QAP 3
+
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
+global.DEBUG = true;
 
+const customerRouter = require("./routes/customer");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.use("/customer", customerRouter);
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`App running on ${PORT}`);
 });
